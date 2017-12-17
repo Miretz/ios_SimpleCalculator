2634e8f177eba861055c77964e473547ae37145c
miretz <miretz@gmail.com>
2017-12-17 16:39:32 +0100

    Initial Commit

---
+++ SimpleCalculator/ViewController.swift
@@ -0,0 +1,121 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by Miretz Dev on 16/12/2017.
+//  Copyright © 2017 Miretz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var numberOnScreen = 0.0 {
+        didSet {
+            let formatter = NumberFormatter()
+            formatter.minimumIntegerDigits = 1
+            formatter.minimumFractionDigits = 0
+            formatter.maximumFractionDigits = 10
+            display.text = formatter.string(from: numberOnScreen as NSNumber) ?? "0"
+        }
+    }
+    
+    var previousNumber = 0.0
+    var shouldClearBeforeInput = false
+    
+    var currentOperation = Operation.none
+    enum Operation {
+        case none
+        case divide
+        case multiply
+        case minus
+        case plus
+    }
+    
+    @IBOutlet weak var display: UILabel!
+    
+    @IBAction func deleteButtonPressed(_ sender: UIButton) {
+        if let text = display.text {
+            let substring = String(text.dropLast())
+            numberOnScreen = Double(substring.isEmpty ? "0" : substring) ?? 0.0
+        }
+    }
+    
+    @IBAction func dotButtonPressed(_ sender: UIButton) {
+        if let text = display.text, !text.contains(".") {
+            display.text = text + "."
+        }
+    }
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+        if let text = display.text {
+
+            let enteredNumber = String(sender.tag-1) // tag is number + 1
+            
+            //clear if there's only a 0, replicating how standard calculators work
+            let displayText = (shouldClearBeforeInput || text=="0") ? enteredNumber : text + enteredNumber
+            shouldClearBeforeInput = false
+
+            numberOnScreen = Double(displayText) ?? 0.0
+        }
+    }
+    
+    private func performCurrentOperation(){
+        switch currentOperation {
+        case .none: return
+        case .divide:
+            if numberOnScreen != 0 {
+                numberOnScreen = previousNumber / numberOnScreen
+            } else {
+                resetCalculator() // dividing by zero will reset the calculator
+            }
+        case .multiply:
+            numberOnScreen = previousNumber * numberOnScreen
+        case .minus:
+            numberOnScreen = previousNumber - numberOnScreen
+        case .plus:
+            numberOnScreen = previousNumber + numberOnScreen
+        }
+        previousNumber = 0.0
+        currentOperation = Operation.none
+    }
+    
+    private func resetCalculator(){
+        previousNumber = 0.0
+        currentOperation = Operation.none
+        numberOnScreen = 0.0
+    }
+    
+    @IBAction func operationPressed(_ sender: UIButton) {
+        
+        //execute the last operation before creating a new one
+        if currentOperation != Operation.none {
+            performCurrentOperation()
+        }
+        
+        switch (sender.tag) {
+        case 11: // reset
+            resetCalculator()
+        case 16: // =
+            performCurrentOperation()
+        case 12: // divide
+            previousNumber = Double(display.text!)!
+            currentOperation = Operation.divide
+        case 13: // multiply
+            previousNumber = Double(display.text!)!
+            currentOperation = Operation.multiply
+        case 14: // minus
+            previousNumber = Double(display.text!)!
+            currentOperation = Operation.minus
+        case 15: // plus
+            previousNumber = Double(display.text!)!
+            currentOperation = Operation.plus
+        default: break
+        }
+        
+        shouldClearBeforeInput = true
+        
+    }
+    
+}
+
